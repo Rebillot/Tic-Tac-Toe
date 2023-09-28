8cc3d259018f5d68aa978aa062e82901a9fd4220
Rebillot <jonathan.rebillot@gmail.com>
2023-09-28 13:01:43 -0300

    Basic game done, need to add a bot to play against

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function App() {
+export default function TicTacToeApp() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setIsXNext(true);
+  }
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const handleMove = (index) => {
+    if (board[index] || calculateWinner(board)) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
+  const winner = calculateWinner(board);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Tic-Tac-Toe</Text>
+      <View style={styles.board}>
+        {board.map((cell, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.cell}
+            onPress={() => handleMove(index)}
+          >
+            <Text style={styles.cellText}>{cell}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      {winner && <Text style={styles.winner}>Winner: {winner}</Text>}
+      <Text>Next player: {isXNext ? 'X' : 'O'}</Text>
+
+      <TouchableOpacity style={styles.resetButton} onPress={resetGame}>
+        <Text style={styles.resetButtonText}>Reset Game</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +72,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  board: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: 300,
+    marginTop: 20,
+  },
+  cell: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+  cellText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  winner: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  resetButton: {
+    backgroundColor: 'blue',
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 5,
+  },
+  resetButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
